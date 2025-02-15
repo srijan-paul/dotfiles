443ecb9059ad5150236a0cdbea70b3dbe6148f42
injuly <srijannr1@gmail.com>
2025-02-15 23:46:59 +0530

    stuff

--- fish/config.fish
+++ fish/config.fish
@@ -2,11 +2,8 @@ function fish_greeting
 	echo (set_color yellow; echo "(Orc peon voice)"; set_color normal) work work...
 end
 
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
 set --export EDITOR nvim
+
 
 # Alias for git commands
 alias gc "git commit"
@@ -18,13 +15,26 @@ alias ghci-core="ghci -ddump-simpl -dsuppress-idinfo \
 -dsuppress-coercions -dsuppress-type-applications \
 -dsuppress-uniques -dsuppress-module-prefixes"
 
-
 # Zig version manager
 set --export ZVM_INSTALL "$HOME/.zvm/self"
 fish_add_path "$HOME/.zvm/bin"
 fish_add_path "$ZVM_INSTALL"
 fish_add_path "$HOME/.local/bin"
 
+# Golang
+fish_add_path "$GOPATH/bin"
+
 # NodeJS (...yeah)
 fish_add_path "$HOME/node/bin"
 
+# pnpm
+set -gx PNPM_HOME "/home/injuly/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
+
+# Jam parser test suite
+set --export JAM_TESTS_262_DIR "$HOME/code/test262-parser-tests"
+# ghcup
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /home/injuly/.ghcup/bin
