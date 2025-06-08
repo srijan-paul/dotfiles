af59884a73b9b15f6d2daf39e6aacca329a82e5b
injuly <srijannr1@gmail.com>
2025-06-08 10:14:46 +0530

    add lean support and clear stale fish config

--- fish/config.fish
+++ fish/config.fish
@@ -32,7 +32,10 @@ fish_add_path "$HOME/.local/bin"
 fish_add_path "$HOME/node/bin"
 fish_add_path /opt/homebrew/bin
 
-if test(uname) = "Linux"
+# Elan and Lean4
+fish_add_path "$HOME/.elan/bin"
+
+if test (uname) = "Linux"
     # GHCUP
     set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /home/injuly/.ghcup/bin
     # pnpm
@@ -49,37 +52,10 @@ else
     # fnm (for switching between nodejs versions)
     set -x FNM_PATH "$HOME/Library/Application Support/fnm"
     fish_add_path "$FNM_PATH"
-    eval $(fnm env)
+    # eval $(fnm env)
 end
-
 
 set -gx VOLTA_HOME "$HOME/.volta"
 set -gx PATH "$VOLTA_HOME/bin" $PATH
-
-if test (uname) = "Darwin"
-    # Gcloud stuff
-    # Gcloud SDK (For prasso)
-    fish_add_path "$HOME/.yen/bin"
-    fish_add_path "$HOME/third-party/google-cloud-sdk/bin"
-
-    # Needed for some scripts I use to test prasso stuff
-    set -x PRASSO_BACK_REPO_PATH "$HOME/prasso/back"
-
-    # The next line updates PATH for the Google Cloud SDK.
-    if [ -f '/Users/injuly/third-party/google-cloud-sdk/path.fish.inc' ]
-        . "/Users/injuly/third-party/google-cloud-sdk/path.fish.inc"
-    end
-
-    set -gx CLOUDSDK_PYTHON "$HOME/.yen_pythons/3.11.11/python/bin/python3"
-    # Helper scripts for prasso work
-    fish_add_path "$HOME/prasso/utils"
-
-end
-
-if type -q go
-    fish_add_path $(go env GOPATH)
-    fish_add_path $(go env GOPATH)/bin
-end
-
 set --export JAM_TESTS_262_DIR "$HOME/code/test262-parser-tests"
 
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -5,7 +5,8 @@ return {
 		require('nvim-treesitter.configs').setup {
 			ensure_installed = {
 				'typescript', 'lua', 'javascript',
-				'c', 'zig', 'json', 'html', 'python'
+				'c', 'zig', 'json', 'html', 'python',
+                'markdown', 'bash', 'css'
 			},
 			highlight = { enable = true },
 			indent = { enable = true },
