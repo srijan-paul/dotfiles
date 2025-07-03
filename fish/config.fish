83d68cc6a55f88c6c3dc9b14fae76befb5f44f68
injuly <srijannr1@gmail.com>
2025-07-03 13:20:47 +0530

    refactor: fish config + nvim fzf setup

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,5 @@
 function fish_greeting 
-	echo (set_color yellow; echo "(Orc peon voice)"; set_color normal) work work...
+	echo (set_color yellow; echo "(Orc peon voice)"; set_color normal) work work…
 end
 
 function fish_prompt
@@ -8,8 +8,10 @@ function fish_prompt
 	     "\$ "
 end
 
-
-set --export EDITOR nvim
+# If nvim is installed, set it as the default editor.
+if type -q nvim
+    set --export EDITOR nvim
+end
 
 # Alias for git commands
 alias gc "git commit"
@@ -18,44 +20,72 @@ alias gcs "git commit -S"
 alias gp "git push"
 alias gs "git status"
 
-alias ghci-core="ghci -ddump-simpl -dsuppress-idinfo \
--dsuppress-coercions -dsuppress-type-applications \
--dsuppress-uniques -dsuppress-module-prefixes"
+# Zig version manager: ~/.zvm
+if test -d "$HOME/.zvm"
+    set --export ZVM_INSTALL "$HOME/.zvm/self"
+    fish_add_path "$HOME/.zvm/bin"
+    fish_add_path "$ZVM_INSTALL"
+    fish_add_path "$HOME/.local/bin"
+end
 
-# Zig version manager
-set --export ZVM_INSTALL "$HOME/.zvm/self"
-fish_add_path "$HOME/.zvm/bin"
-fish_add_path "$ZVM_INSTALL"
-fish_add_path "$HOME/.local/bin"
 
-# NodeJS (...yeah)
-fish_add_path "$HOME/node/bin"
-fish_add_path /opt/homebrew/bin
+# NodeJS
+if test -d "$HOME/node"
+    fish_add_path "$HOME/node/bin"
+end
 
-# Elan and Lean4
-fish_add_path "$HOME/.elan/bin"
+# Lean
+if test -d "$HOME/.elan"
+    fish_add_path "$HOME/.elan/bin"
+end 
+
+# Haskell toolchain
+if test -d "$HOME/.ghcup"
+    fish_add_path "$HOME/.ghcup/bin"
+    # "ghci-core" launches GHCi with options that print the GHC Core
+    # IR for every line entered in the REPL.
+    alias ghci-core="ghci -ddump-simpl -dsuppress-idinfo \
+    -dsuppress-coercions -dsuppress-type-applications \
+    -dsuppress-uniques -dsuppress-module-prefixes"
+end
 
 if test (uname) = "Linux"
-    # GHCUP
+    # Linux config: Desktop at home and anything I ssh into.
+
     set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /home/injuly/.ghcup/bin
-    # pnpm
+
+    # PNPM: this part was added by the pnpm installer.
     set -gx PNPM_HOME "/home/injuly/.local/share/pnpm"
     if not string match -q -- $PNPM_HOME $PATH
       set -gx PATH "$PNPM_HOME" $PATH
     end
-    # pnpm end
 else
-    # gchup
-    # Honestly, ghcup takes up WAY too much space (~5G on a fresh install).
-    fish_add_path "$HOME/.ghcup/bin/"
+    # MacOS config: for my personal laptop and work device.
 
-    # fnm (for switching between nodejs versions)
+    fish_add_path /opt/homebrew/bin
     set -x FNM_PATH "$HOME/Library/Application Support/fnm"
     fish_add_path "$FNM_PATH"
-    # eval $(fnm env)
+    
+    # Local OCCT install directory.
+    if test -d "$HOME/code/OCCT" 
+        set -x OCCT_RESOURCE_PATH $HOME/code/occt/build/install/share/opencascade/resources
+        set -x OCCT_SHARE_PATH $HOME/code/occt/build/install/share
+        set -x DYLD_LIBRARY_PATH $HOME/code/occt/build/install/lib
+        fish_add_path "$HOME/code/OCCT/build/install/bin"
+    end
+
+    # PNPM: this part was added by the pnpm installer.
+    set -gx PNPM_HOME "/Users/srijan/Library/pnpm"
+    if not string match -q -- $PNPM_HOME $PATH
+      set -gx PATH "$PNPM_HOME" $PATH
+    end
+
 end
 
+# Added by volta installer (i think).
 set -gx VOLTA_HOME "$HOME/.volta"
 set -gx PATH "$VOLTA_HOME/bin" $PATH
+
+# Jam: a WIP js toolchain I'm working on.
 set --export JAM_TESTS_262_DIR "$HOME/code/test262-parser-tests"
 
--- nvim/lua/injuly/keybinds.lua
+++ nvim/lua/injuly/keybinds.lua
@@ -1,11 +1,11 @@
 vim.g.mapleader = " " -- leader key is space
 
 local function map(mode, lhs, rhs, opts)
-	local options = { noremap = true }
-	if opts then
-		options = vim.tbl_extend("force", options, opts)
-	end
-	vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+    local options = { noremap = true }
+    if opts then
+        options = vim.tbl_extend("force", options, opts)
+    end
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
 map("n", "<C-n>", ":tabnew<CR>")
@@ -14,6 +14,12 @@ local km = vim.keymap
 
 -- FzfLua keybinds
 local fzf = require("fzf-lua")
+fzf.setup({
+    files = {
+        provider = "rg",
+        fd_opts = "--type f --color=always --hidden --follow --exclude .git"
+    }
+})
 km.set("n", "<Leader>ff", fzf.files, { desc = "Fzf files" })
 km.set("n", "<Leader>fg", fzf.live_grep_native, { desc = "Live grep" })
 km.set("n", "<Leader>qf", fzf.quickfix, { desc = "Quick fixes" })
@@ -26,4 +32,8 @@ km.set("n", "<Leader>m", fzf.marks, { desc = "Marks" })
 local textcase = require("textcase");
 km.set("n", "<Leader>toc", function() textcase.current_word('to_camel_case') end)
 km.set("n", "<Leader>tos", function() textcase.current_word('to_snake_case') end)
+
+-- Copilot accept override
+vim.g['copilot_no_tab_map'] = true
+map('i', '<C-j>', 'copilot#Accept("<CR>")', { expr = true, silent = true })
 
